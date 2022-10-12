f38e9cf88a2223e23e9dcce6fb80a96c14988681
Ben Holman <58403382+Ben-Holman@users.noreply.github.com>
2022-10-12 15:33:09 -0400

    Update Source.cpp

--- Holman_ex51/Holman_ex51/Source.cpp
+++ Holman_ex51/Holman_ex51/Source.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include "LinkedNode.h"
 
+// Ben Holman 
+// First Edit: 10/7/2022
+// Last Edit:  10/12/2022
+
+// if you input a character for an integer you will get an infinite loop.
+
 int main() {
 	int input; 
-	char answer = 0; 
-	linkedList* node;
-	ListNode* node1;
-	while (answer != 'y' && answer != 'Y') {
+	linkedList* node = NULL;
+	ListNode* node1 = NULL;
+	while (true) {
+		char answer;
 		std::cout << "Insert a value into the Linked List:\n"; 
 		std::cin >> input; 
 		node->insertFront(&node1, input);
-		std::cout << "Value inputted into the Linked List. Would you like to Insert another value?\n";
+		std::cout << "Value inputted into the Linked List. Would you like to Insert another value? (Y/N)\n";
 		std::cin >> answer; 
+		if (answer == 'N' || answer == 'n') {
+			break;
+		}
+		else {
+			continue;
+		}
+
 	}
 	node->printList(node1);
 
